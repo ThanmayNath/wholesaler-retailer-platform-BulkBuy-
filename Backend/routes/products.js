c4603e017221578c31f626f56ede2bb1f31d8881
Thanmay Nath <thanmaynath357@gmail.com>
2023-05-21 18:42:25 +0530

    backend 2nd commit

---
+++ Backend/routes/products.js
@@ -0,0 +1,52 @@
+import { Router } from "express";
+const router = Router();
+import db from "../db/config.js";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    console.log(file);
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.get("/", async (req, res) => {
+  const query = `SELECT * FROM public.products`;
+  try {
+    const data = await db.query(query);
+    const result = data.rows;
+    return res.send(result).status(200);
+  } catch (error) {}
+});
+
+router.post("/upload", upload.single("file"), async (req, res) => {
+  const values = [
+    req.body.product_id,
+    req.body.wholesaler_id,
+    req.body.category_id,
+    req.body.product_name,
+    req.body.product_description,
+    req.body.product_price,
+    req.body.product_quantity,
+    req.file.path,
+  ];
+  const query = `INSERT INTO public.products(
+        product_id, wholesaler_id, category_id, product_name, product_description, product_price, product_quantity, product_image_url)
+        VALUES ($1, $2, $3, $4, $5 , $6, $7, $8)`;
+  try {
+    await db.query(query, values);
+    res.status(200).json({ mesage: "Uploaded the product successfully." });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while uploading the product." });
+  }
+});
+
+export default router;
--- Backend/routes/retailerAuthentication.js
+++ Backend/routes/retailerAuthentication.js
@@ -5,11 +5,6 @@ import db from "../db/config.js";
 
 router.post("/reg", async (req, res) => {
   try {
-    /*const retailer_name = req.body.retailer_name;
-    const retailer_email = req.body.retailer_email;
-    const retailer_password = req.body.retailer_password;
-    const retailer_address = req.body.retailer_address;
-    const retailer_city = req.body.retailer_city;*/
     const retailer_password = req.body.retailer_password;
     const salt = bcrypt.genSaltSync(10);
     const hashedPassword = bcrypt.hashSync(retailer_password, salt);
