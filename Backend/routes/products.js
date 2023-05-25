9105d1d2900bf913606bdcb3cdfc1050493ebdc7
Thanmay Nath <thanmaynath357@gmail.com>
2023-05-25 20:41:51 +0530

    backend 2nd commit

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage }).array("files", 3); // Allow uploading up to 3 files
 
 router.get("/", async (req, res) => {
   const query = `SELECT * FROM public.products`;
@@ -26,28 +26,45 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/upload", upload.single("file"), async (req, res) => {
-  const values = [
-    req.body.wholesaler_id,
-    req.body.category_id,
-    req.body.product_name,
-    req.body.product_description,
-    req.body.product_price,
-    req.body.product_quantity,
-    req.file.path,
-  ];
-  const query = `INSERT INTO public.products(
-        wholesaler_id, category_id, product_name, product_description, product_price, product_quantity, product_image_url)
-        VALUES ($1, $2, $3, $4, $5 , $6, $7)`;
-  try {
-    await db.query(query, values);
-    res.status(200).json({ mesage: "Uploaded the product successfully." });
-  } catch (error) {
-    console.log(error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while uploading the product." });
-  }
+router.post("/upload", (req, res) => {
+  upload(req, res, async (err) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(500)
+        .json({ error: "An error occurred while uploading the images." });
+    }
+
+    const files = req.files; // Array of uploaded files
+    const filePaths = files.map((file) => file.path);
+
+    const values = [
+      req.body.wholesaler_id,
+      req.body.category_id,
+      req.body.product_name,
+      req.body.product_description,
+      req.body.product_price,
+      req.body.product_quantity,
+      filePaths[0], // First image URL
+      filePaths[1], // Second image URL
+      filePaths[2], // Third image URL
+    ];
+
+    const query = `INSERT INTO public.products(
+      wholesaler_id, category_id, product_name, product_description, product_price, product_quantity, 
+      product_image_url, product_image_url2, product_image_url3)
+      VALUES ($1, $2, $3, $4, $5 , $6, $7, $8, $9)`;
+
+    try {
+      await db.query(query, values);
+      res.status(200).json({ message: "Uploaded the product successfully." });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while uploading the product." });
+    }
+  });
 });
 
 export default router;
